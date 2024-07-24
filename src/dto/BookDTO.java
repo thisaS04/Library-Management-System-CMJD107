dae762dbec128cfc753a8975d154f4a1d94d20bd
thisaS04 <thisaranisenevi@gmail.com>
2024-07-24 11:49:30 +0530

    BookDto Committed

--- src/dto/BookDTO.java
+++ src/dto/BookDTO.java
@@ -1,5 +1,58 @@
 package dto;
 
-public class BookDTO {
+public class BookDto {
+    private int id;
+    private String title;
+    private String author;
+    private Long categoryId;
+    private boolean available;
+
+ public BookDto(){
+
+ }
+public BookDto(int id, String title,String author,Long categoryId,boolean available ){
+    this.id=id;
+    this.title=title;
+    this.author=author;
+    this.categoryId=categoryId;
+    this.available=available;
     
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getTitle() {
+    return title;
+}
+public void setTitle(String title) {
+    this.title = title;
+}
+public String getAuthor() {
+    return author;
+}
+public void setAuthor(String author) {
+    this.author = author;
+}
+public Long getCategoryId() {
+    return categoryId;
+}
+public void setCategoryId(Long categoryId) {
+    this.categoryId = categoryId;
+}
+public boolean isAvailable() {
+    return available;
+}
+public void setAvailable(boolean available) {
+    this.available = available;
+}
+@Override
+public String toString() {
+    return "BookDto [id=" + id + ", title=" + title + ", author=" + author + ", categoryId=" + categoryId
+            + ", available=" + available + "]";
+}
+
+
 }
